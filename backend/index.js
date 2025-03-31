01744dd5d3f8c9b823a185259ec9dac55fdb4931
Winstone Were <stoniedev@gmail.com>
2025-03-31 12:21:23 +0300

    testing CI/CD

--- backend/index.js
+++ backend/index.js
@@ -12,5 +12,9 @@ app.get("/", (req, res) => {
   res.send("AI Vibe Music Backend is Running!");
 });
 
+app.get("/test",(req,res)=>{
+  res.send("Deployeeddd");
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
